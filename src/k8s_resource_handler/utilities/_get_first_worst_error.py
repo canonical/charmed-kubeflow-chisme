9e56829d3ea9a63d038a3f550e9d930995862069
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-08 16:31:07 -0400

    add get_first_word_error utility for handling lists of errors

---
+++ src/k8s_resource_handler/utilities/_get_first_worst_error.py
@@ -0,0 +1,27 @@
+from ops.model import WaitingStatus, BlockedStatus
+from typing import List
+
+from ..exceptions import ErrorWithStatus
+
+
+def get_first_worst_error(errors: List[ErrorWithStatus]) -> ErrorWithStatus:
+    """Returns the first of the worst errors in the list, ranked by their status
+
+    Raises if List contains no Exceptions, or if any Exception does not have a .status
+
+    Status are ranked, starting with the worst:
+        BlockedStatus
+        WaitingStatus
+    """
+    cached_error = None
+
+    for error in errors:
+        if error is None:
+            continue
+
+        if isinstance(error.status, BlockedStatus):
+            return error
+        elif isinstance(error.status, WaitingStatus):
+            cached_error = error
+
+    return cached_error
