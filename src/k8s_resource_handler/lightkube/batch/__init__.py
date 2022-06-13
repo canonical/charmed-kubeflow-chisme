dd7fc2d5ab46e23a64bd701b25ea22f784b521bb
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-13 09:33:40 -0400

    Fix imports for _sort_objects

--- src/k8s_resource_handler/lightkube/batch/__init__.py
+++ src/k8s_resource_handler/lightkube/batch/__init__.py
@@ -3,5 +3,5 @@
 
 """Utilities for working on batches of resources using the Lightkube library"""
 
-from _sort_objects import _sort_objects
-from _many import apply_many, delete_many
+from ._sort_objects import _sort_objects
+from ._many import apply_many, delete_many
