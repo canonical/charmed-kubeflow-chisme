43bc94134a1b43a048d2773a0b3e8cc25d5bbfc6
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-13 08:28:08 -0400

    Add helper sort_objects until it is merged into upstream lightkube

---
+++ src/k8s_resource_handler/lightkube/batch/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Utilities for working on batches of resources using the Lightkube library"""
+
+from _sort_objects import sort_objects
