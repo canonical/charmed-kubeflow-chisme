ba774c581705747ff1f00c83cbbd4ca6680b0888
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-14 18:10:43 -0400

    Update exceptions.__init__.py docstring per review feedback
    Co-authored-by: Daniela Plascencia <daniela.plascencia@canonical.com>

--- src/k8s_resource_handler/exceptions/__init__.py
+++ src/k8s_resource_handler/exceptions/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""A collection of standard Exceptions used across the Charmed Kubeflow charms."""
+"""A collection of standard Exceptions for use when writing charms."""
 
 from ._kubernetes import ReconcileError, ReplicasNotReadyError, ResourceNotFoundError
 from ._with_status import ErrorWithStatus
