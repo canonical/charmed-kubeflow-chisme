f4a737119bf83641794f5826d656356337b1aee3
Andrew Scribner <ca.scribner+1@gmail.com>
2022-09-16 11:17:41 -0400

    feat: add tools for interacting with bundles and juju CLI
    Bundle class adds things like loading bundles from yaml, comparing bundles, diffing bundles.  Also included is the get_newest_charm_revision() helper, which retrieves from `juju info` cli command what the newest charm revision is for a charm in a particular channel.
    
    Juju class and helpers add some convenience functions and structure around calling the juju cli

---
+++ src/charmed_kubeflow_chisme/bundle/_bundle.py
@@ -0,0 +1,91 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Helper to work with Juju bundles, preserving comments in the YAML"""
+
+import copy
+from pathlib import Path
+from typing import Optional
+
+from deepdiff import DeepDiff
+from ruamel.yaml import YAML
+
+from ..juju import Juju
+
+
+class Bundle:
+    """Juju bundle loader/dumper.
+
+    This class uses ruamel.yaml instead of the typical pyyaml in order to preserve all comments,
+    the order of the yaml, etc.  This way we can load/dump a yaml with comments without losing them
+    or reordering the items.
+    """
+
+    def __init__(self, filename: Optional[str] = None):
+        self._filename = filename
+        self._data = None
+        if self._filename:
+            self.load_bundle()
+
+    def deepcopy(self):
+        """Returns a new deep copy of this Bundle"""
+        newbundle = copy.deepcopy(self)
+        return newbundle
+
+    def diff(self, other):
+        """Returns a diff between this and another object, in DeepDiff format"""
+        return DeepDiff(self._data, other._data, ignore_order=True)
+
+    def dump(self, filename: str):
+        """Dumps as yaml to a file"""
+        with open(filename, "w") as fout:
+            yaml = YAML(typ="rt")
+            yaml.dump(self.to_dict(), fout)
+
+    def get_latest_revisions(self):
+        """Return a new Bundle that has the latest revisions of all charms in this bundle"""
+        newbundle = self.deepcopy()
+
+        applications = newbundle.applications
+
+        for name, application in applications.items():
+            # Skip charms we're not "tracking" a channel for
+            if "_tracked_channel" not in application:
+                continue
+
+            charm_name = application["charm"]
+            channel = application["_tracked_channel"]
+            revision = application.get("revision", None)
+
+            newest_revision = get_newest_charm_revision(charm_name, channel)
+
+            if revision != newest_revision:
+                # If revision is not up to date - update in situ from tracked channel
+                application["revision"] = newest_revision
+
+        return newbundle
+
+    def load_bundle(self):
+        """Loads a YAML file as a bundle"""
+        yaml = YAML(typ="rt")
+        self._data = yaml.load(Path(self._filename).read_text())
+
+    def to_dict(self):
+        return self._data
+
+    def __eq__(self, other):
+        return self._filename == other._filename and self.diff(other) == {}
+
+    @property
+    def applications(self):
+        return self._data["applications"]
+
+
+def get_newest_charm_revision(charm: str, channel: str):
+    """Returns the newest revision of a charm in a channel"""
+    charm_info = Juju.info(charm)
+    channel_map = charm_info["channel-map"]
+    try:
+        tracked_channel_release = channel_map[channel]
+    except KeyError:
+        raise ValueError(f"No channel {channel} in `juju info {charm} --format yaml`")
+    return tracked_channel_release["revision"]
