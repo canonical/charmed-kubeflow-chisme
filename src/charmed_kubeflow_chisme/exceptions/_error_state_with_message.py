6c0192950c06b758c7cf4e390c5859e1fb2fe4ff
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-03-17 11:50:15 +0100

    [skip] fix module

--- src/charmed_kubeflow_chisme/exceptions/_error_state_with_message.py
+++ src/charmed_kubeflow_chisme/exceptions/_error_state_with_message.py
@@ -5,6 +5,6 @@ class UnitErrorStatusWithMessage(Exception):  # noqa: N818
     """Raised when the unit should be in ErrorStatus with a message to show in juju status."""
 
     def __init__(self, msg: str):
-        __module__ = 'builtins'
+        __module__ = None
         super().__init__(str(msg))
         self.msg = str(msg)
