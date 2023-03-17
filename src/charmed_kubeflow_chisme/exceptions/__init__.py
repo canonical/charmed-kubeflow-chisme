7d722f6474ce04b4f840d5f43607ee66e371e133
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-03-17 11:53:31 +0100

    [skip] fix minor issues with linting

--- src/charmed_kubeflow_chisme/exceptions/__init__.py
+++ src/charmed_kubeflow_chisme/exceptions/__init__.py
@@ -3,8 +3,13 @@
 
 """A collection of standard Exceptions for use when writing charms."""
 
+from ._error_status_with_message import UnitErrorStatusWithMessage
 from ._kubernetes import ReplicasNotReadyError, ResourceNotFoundError
 from ._with_status import ErrorWithStatus
-from ._error_status_with_message import UnitErrorStatusWithMessage
 
-__all__ = [ReplicasNotReadyError, ResourceNotFoundError, ErrorWithStatus]
+__all__ = [
+    ReplicasNotReadyError,
+    ResourceNotFoundError,
+    ErrorWithStatus,
+    UnitErrorStatusWithMessage,
+]
--- src/charmed_kubeflow_chisme/exceptions/_error_status_with_message.py
+++ src/charmed_kubeflow_chisme/exceptions/_error_status_with_message.py
@@ -1,10 +1,12 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+
 class UnitErrorStatusWithMessage(Exception):  # noqa: N818
     """Raised when the unit should be in ErrorStatus with a message to show in juju status."""
 
+    __module__ = None
+
     def __init__(self, msg: str):
-        __module__ = None
         super().__init__(str(msg))
         self.msg = str(msg)
