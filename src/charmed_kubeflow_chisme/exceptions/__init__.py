17f522304175a085132ef1b700207bef4fe3586b
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-03-17 14:25:37 +0100

    feat: add custom exception for ErrorStatus with message (#39)
    * feat: add custom exception for ErrorStatus with message
    
    This exception can be raised by the charm code when the unit
    has to go in ErrorStatus mode and the message displayed in
    juju status has to be informative.

--- src/charmed_kubeflow_chisme/exceptions/__init__.py
+++ src/charmed_kubeflow_chisme/exceptions/__init__.py
@@ -3,7 +3,13 @@
 
 """A collection of standard Exceptions for use when writing charms."""
 
+from ._generic_charm_runtime_error import GenericCharmRuntimeError
 from ._kubernetes import ReplicasNotReadyError, ResourceNotFoundError
 from ._with_status import ErrorWithStatus
 
-__all__ = [ReplicasNotReadyError, ResourceNotFoundError, ErrorWithStatus]
+__all__ = [
+    ReplicasNotReadyError,
+    ResourceNotFoundError,
+    ErrorWithStatus,
+    GenericCharmRuntimeError,
+]
