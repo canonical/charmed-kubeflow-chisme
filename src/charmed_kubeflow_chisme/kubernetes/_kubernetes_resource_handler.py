29a30ce7f9c64f9ebdb06d39ab395fc668fb6c47
Andrew Scribner <ca.scribner+1@gmail.com>
2023-04-19 15:28:42 -0400

    fix: formatting

--- src/charmed_kubeflow_chisme/kubernetes/_kubernetes_resource_handler.py
+++ src/charmed_kubeflow_chisme/kubernetes/_kubernetes_resource_handler.py
@@ -14,7 +14,11 @@ from ops.model import ActiveStatus, BlockedStatus
 from ..exceptions import ErrorWithStatus
 from ..lightkube.batch import apply_many, delete_many
 from ..status_handling import get_first_worst_error
-from ..types import LightkubeResourcesList, LightkubeResourceType, LightkubeResourceTypesList
+from ..types import (
+    LightkubeResourcesList,
+    LightkubeResourceType,
+    LightkubeResourceTypesList,
+)
 from ..types._charm_status import AnyCharmStatus
 from ._check_resources import check_resources
 
