d615e56b1c50e2a421fd2281786a90a35aecf449
Ivan Chvets <ivan.chvets@canonical.com>
2023-05-31 17:17:15 -0400

    fix: added version exraction function

--- src/charmed_kubeflow_chisme/rock/__init__.py
+++ src/charmed_kubeflow_chisme/rock/__init__.py
@@ -4,5 +4,6 @@
 """Utilities for Handling Rocks in Charms and Tests."""
 
 from ._test_rock import get_rock_image_name_from_rockcraft
+from ._test_rock import get_rock_image_version_from_rockcraft
 
-__all__ = [get_rock_image_name_from_rockcraft]
+__all__ = [get_rock_image_name_from_rockcraft, get_rock_image_version_from_rockcraft]
