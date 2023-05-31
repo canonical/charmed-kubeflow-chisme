60fb7663836ce806941b371dc21e2dd9f6bb75ec
Ivan Chvets <ivan.chvets@canonical.com>
2023-05-31 10:35:55 -0400

    feat: test rock class
    Summary of changes:
    - Added TestRock class.

---
+++ src/charmed_kubeflow_chisme/rock/README.md
@@ -0,0 +1,4 @@
+# TestRock
+
+TestRock class provides utilities for testing ROCK images.
+
---
+++ src/charmed_kubeflow_chisme/rock/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Utilities for Handling Rocks in Charms and Tests."""
+
+from ._test_rock import TestRock
+
+__all__ = [TestRock]
+
