e0954be8a4abf16a076cd510b0d980d68c6f82b6
Ivan Chvets <ivan.chvets@canonical.com>
2023-05-31 15:37:38 -0400

    feat: added version extraction function

--- src/charmed_kubeflow_chisme/rock/_test_rock.py
+++ src/charmed_kubeflow_chisme/rock/_test_rock.py
@@ -5,10 +5,14 @@ from pathlib import Path
 
 import yaml
 
+def get_rock_image_version_from_rockcraft(file: str):
+    version = rockcraft["version"]
+    return version
+
 def get_rock_image_name_from_rockcraft(file: str):
     """Reads a rockcraft.yaml file and returns the ROCK image name."""
     rockcraft = yaml.safe_load(Path(file).read_text())
     name = rockcraft["name"]
-    version = rockcraft["version"]
+    version = get_rock_image_version_from_rockcraft(file)
     arch = list(rockcraft["platforms"].keys())[0]
-    return f"{name}_{version}_{arch}:{version}"
+    return f"{name}_{version}_{arch}"
