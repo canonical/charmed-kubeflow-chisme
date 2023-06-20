c82bd1dd21b375c6cf91f43298fdc09d98e4e07c
Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>
2023-06-20 20:14:45 +0300

    feat: Add error handling for deleting resources (#62)
    Capture `ApiErrors` when attempting to delete K8s resources and provide an option to ignore 404 errors, since they don't affect the deletion. Catch everything else and raise it at the end, after deletion is attempted for all detected resources. 
    
    Add logging to the `apply_many` and `delete_many` helpers.
    
    Signed-off-by: Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>

--- src/charmed_kubeflow_chisme/lightkube/batch/_many.py
+++ src/charmed_kubeflow_chisme/lightkube/batch/_many.py
@@ -1,14 +1,18 @@
-# Copyright 2022 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import logging
 from typing import Iterable, TypeVar, Union
 
 import lightkube
 from lightkube.core import resource
+from lightkube.core.exceptions import ApiError
 from lightkube.core.resource import GlobalResource, NamespacedResource
 
 # Replace with lightkube.sort_objects once gtsystem/lightkube#33 is merged
 from ._sort_objects import _sort_objects as sort_objects
+
+LOGGER = logging.getLogger(__name__)
 
 GlobalResourceTypeVar = TypeVar("GlobalResource", bound=resource.GlobalResource)
 GlobalSubResourceTypeVar = TypeVar("GlobalSubResource", bound=resource.GlobalSubResource)
@@ -20,6 +24,7 @@ def apply_many(
     objs: Iterable[Union[GlobalResourceTypeVar, NamespacedResourceTypeVar]],
     field_manager: str = None,
     force: bool = False,
+    logger: logging.Logger = None,
 ) -> Iterable[Union[GlobalResourceTypeVar, NamespacedResourceTypeVar]]:
     """Create or configure an iterable of Lightkube objects using client.apply().
 
@@ -37,17 +42,19 @@ def apply_many(
 
     Args:
         client: Lightkube client to use for applying resources
-        objs:  iterable of objects to create. This need to be instances of a resource kind and have
-               resource.metadata.namespaced defined if they are namespaced resources
+        objs: Iterable of objects to create. This need to be instances of a resource kind and have
+              resource.metadata.namespaced defined if they are namespaced resources
         field_manager: Name associated with the actor or entity that is making these changes.
         force: *(optional)* Force is going to "force" Apply requests. It means user will
                re-acquire conflicting fields owned by other people.
+        logger: *(optional)* Logger to use for applying resources
 
     Returns:
         A list of Resource objects returned from client.apply().  This list is returned in the
         order the resources were actually applied, not the order in which they're passed as inputs
         in `objs`.
     """
+    logger = logger or LOGGER
     objs = sort_objects(objs)
     returns = [None] * len(objs)
 
@@ -61,6 +68,7 @@ def apply_many(
                 f"apply_many only supports objects of types NamespacedResource or GlobalResource,"
                 f" got {type(obj)}"
             )
+        logger.debug(f"Creating {obj.__class__} {obj.metadata.name}...")
         returns[i] = client.apply(
             obj=obj, namespace=namespace, field_manager=field_manager, force=force
         )
@@ -70,6 +78,8 @@ def apply_many(
 def delete_many(
     client: lightkube.Client,
     objs: Iterable[Union[GlobalResourceTypeVar, NamespacedResourceTypeVar]],
+    ignore_missing: bool = True,
+    logger: logging.Logger = None,
 ) -> None:
     """Delete an iterable of objects using client.delete().
 
@@ -78,10 +88,14 @@ def delete_many(
 
     Args:
         client: Lightkube Client to use for deletions
-        objs:  iterable of objects to delete. This need to be instances of a resource kind and have
-               resource.metadata.namespaced defined if they are namespaced resources
+        objs: Iterable of objects to delete. This need to be instances of a resource kind and have
+              resource.metadata.namespaced defined if they are namespaced resources
+        ignore_missing: *(optional)* Avoid raising 404 errors on deletion (defaults to True)
+        logger: *(optional)* Logger to use for deleting resources
     """
+    logger = logger or LOGGER
     objs = sort_objects(objs, reverse=True)
+    exceptions = []
 
     for obj in objs:
         if isinstance(obj, NamespacedResource):
@@ -93,5 +107,18 @@ def delete_many(
                 "delete_many only supports objects of types NamespacedResource or GlobalResource,"
                 f" got {type(obj)}"
             )
+        try:
+            logger.debug(f"Deleting {obj.__class__} {obj.metadata.name}...")
+            client.delete(res=obj.__class__, name=obj.metadata.name, namespace=namespace)
+        except ApiError as error:
+            if error.status.code == 404 and ignore_missing:
+                logger.debug(
+                    f"{obj.__class__} {obj.metadata.name} not found! Ignoring because"
+                    f" ignore_missing={ignore_missing}."
+                )
+            else:
+                logger.debug(f"Failed to delete {obj.__class__} {obj.metadata.name}: {error}")
+                exceptions.append(error)
 
-        client.delete(res=obj.__class__, name=obj.metadata.name, namespace=namespace)
+    if exceptions:
+        raise RuntimeError("Deleting K8s resources completed with errors", exceptions)
