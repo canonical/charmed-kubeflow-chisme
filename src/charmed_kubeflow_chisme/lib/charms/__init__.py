3daeb7b95aaaebda1d7a5ff56a612a718385c49a
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-03-13 13:10:36 +0200

    testtt

--- src/charmed_kubeflow_chisme/lib/charms/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2022 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-"""Utilities we need from charmcraft libraries."""
-
-from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
-from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
-
-__all__ = [KubernetesServicePatch, MetricsEndpointProvider]
--- src/charmed_kubeflow_chisme/pebble/__init__.py
+++ src/charmed_kubeflow_chisme/pebble/__init__.py
@@ -5,5 +5,7 @@
 
 from ._update_layer import update_layer
 from ._create_metrics_endpoint import create_metrics_endpoint
+from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
 
-__all__ = [create_metrics_endpoint, update_layer]
+__all__ = [create_metrics_endpoint, update_layer, KubernetesServicePatch, MetricsEndpointProvider]
