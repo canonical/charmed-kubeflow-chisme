5d459b1139a1eb38e49a9213cf6b98ac14905a11
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-03-13 12:52:48 +0200

    fix

--- src/charmed_kubeflow_chisme/lib/charms/__init__.py
+++ src/charmed_kubeflow_chisme/lib/charms/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Utilities for Handling Pebble in Charms."""
+"""Utilities we need from charmcraft libraries."""
 
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
 from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
