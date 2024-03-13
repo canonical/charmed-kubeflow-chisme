b73c4414a26a892e24ed5a978476692214a1010d
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-03-13 12:29:44 +0200

    rename to lib and add init.py

---
+++ src/charmed_kubeflow_chisme/lib/charms/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Utilities for Handling Pebble in Charms."""
+
+from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
+
+__all__ = [KubernetesServicePatch, MetricsEndpointProvider]
